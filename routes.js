4d37f9357d6c7f50f624eaa614cefa615458422e
iamblue <tonyone0902@gmail.com>
2013-07-26 16:25:10 +0800

    update on heroku

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var controllers = require(path.join(__dirname, 'controllers', 'index.js'));
 var models = require(path.join(__dirname, 'models', 'index.js'));
-var config = require(path.join(__dirname, '/', 'config.json'));
+//var config = require(path.join(__dirname, '/', 'config.json'));
 
 module.exports = function (app) {  
   app.get('/', controllers.home.index);
